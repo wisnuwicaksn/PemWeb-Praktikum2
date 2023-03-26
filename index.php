833d7379595ccb4a1985e7b0d6662a93111afbc1
wisnuwicaksn <adiwisnu123@gmail.com>
2023-03-26 18:04:08 +0700

    up

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<!-- <html>
-    <head>
-        <title>Membuat Program Menghitung Umur dengan PHP</title>
-    </head>
-    <body>
-        <form action="hitung_umur.php" method="post">
-            Masukkan Tanggal Lahir : <input type="date" name="tanggal_lahir"><br>
-            <input type="submit" value="Hitung Umur"/>
-        </form>
-    </body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
